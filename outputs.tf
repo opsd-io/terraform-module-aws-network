012d63a960d199dde5c254dad55cef9a7ada4ce8
Piotr Stawarski <piotr.stawarski@zdt.io>
2023-08-18 13:19:21 +0200

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,33 @@
-# output "variable" {
-#   description = "output variable description"
-#   value       = variable.main.name
-# }
+output "availability_zones" {
+  description = "A map of AZ prefixes and their full names."
+  value       = local.availability_zones
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC."
+  value       = aws_vpc.main.id
+}
+output "vpc_arn" {
+  description = "The ARN of the VPC."
+  value       = aws_vpc.main.arn
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway."
+  value       = aws_internet_gateway.main.id
+}
+
+output "internet_gateway_arn" {
+  description = "The ARN of the Internet Gateway."
+  value       = aws_internet_gateway.main.arn
+}
+
+output "vpn_gateway_id" {
+  description = "The ID of the VPN Gateway."
+  value       = aws_vpn_gateway.main.id
+}
+
+output "vpn_gateway_arn" {
+  description = "The ARN of the VPN Gateway."
+  value       = aws_vpn_gateway.main.arn
+}
